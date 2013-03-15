d1da03acb764b40d4d94433ef46dda3cae222aba
Rob Bercik <rob.bercik@gmail.com>
2013-03-15 16:31:54 -0400

    adding initial server changes

--- server/server.py
+++ server/server.py
@@ -1,7 +1,6 @@
 
 import SimpleHTTPServer
 import SocketServer
-import os
 import re
 import urllib
 
@@ -9,17 +8,16 @@ PORT = 8989
 
 class MyServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
-
         request_path = re.sub(r'^.*GET ', '', self.path);
         request_path = re.sub(r'HTTP.*', '', request_path);
         url = urllib.urlopen('http://reddit.com' + request_path);
         print url.info();
-        f = self.wrapFile(url);
+        f = self.wrapStream(url);
         if f:
             self.copyfile(f, self.wfile);
             f.close();
 
-    def wrapFile(self, urlref):
+    def wrapStream(self, urlref):
         self.send_response(200);
         for key in urlref.info().keys():
             self.send_header(key, urlref.info()[key]);
