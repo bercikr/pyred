e1b912a589fc1d6631ed5eb7306a5f2335cf8504
rob <rob@rob-acer-ubuntu.(none)>
2013-03-15 19:44:18 -0400

    hey now

--- client/canvas.js
+++ client/canvas.js
@@ -1,12 +1,22 @@
 function load() {
-
     request('http://localhost:8989/r/pics/.json');
     draw();
 }
 
 function request(url){
     console.log(url);
-    request = new XMLHttpRequest();
+    var request;
+    var READYSTATE_COMPLETE = 4;
+    if (typeof(XMLHttpRequest) == "undefined") {
+        XMLHttpRequest =
+            function()   {
+                try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); } catch(e) {}
+                try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); } catch(e) {}
+                try { return new ActiveXObject("Msxml2.XMLHTTP"); }     catch(e) {}
+                try { return new ActiveXObject("Microsoft.XMLHTTP"); }  catch(e) {}
+                    throw new Error("This browser does not support XMLHttpRequest.");
+            };
+    }
 
 }
 
@@ -19,5 +29,4 @@ function draw() {
         context.drawImage(imageObj, 100, 150);
     };
     imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
-
-}
+};
