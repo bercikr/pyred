abc0ac9b568af37c269ddf9c0f9ad97bc8dc9b4e
rob <rob@rob-acer-ubuntu.(none)>
2013-03-16 14:49:23 -0400

    adding some json basics

--- client/canvas.js
+++ client/canvas.js
@@ -1,23 +1,22 @@
 function load() {
-    request('http://localhost:8989/r/pics/.json');
+    //request('http://localhost:8989/r/pics/.json');
+    request('nada')
     draw();
 }
 
 function request(url){
-    console.log(url);
-    var request;
-    var READYSTATE_COMPLETE = 4;
-    if (typeof(XMLHttpRequest) == "undefined") {
-        XMLHttpRequest =
-            function()   {
-                try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); } catch(e) {}
-                try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); } catch(e) {}
-                try { return new ActiveXObject("Msxml2.XMLHTTP"); }     catch(e) {}
-                try { return new ActiveXObject("Microsoft.XMLHTTP"); }  catch(e) {}
-                    throw new Error("This browser does not support XMLHttpRequest.");
-            };
-    }
+  $.getJSON('test.json', function(data) {
+    var items = [];
 
+    $.each(data, function(key, val) {
+      items.push('<li id="' + key + '">' + val + '</li>');
+    });
+
+    $('<ul/>', {
+      'class': 'my-new-list',
+      html: items.join('')
+    }).appendTo('body');
+  });
 }
 
 function draw() {
@@ -27,6 +26,10 @@ function draw() {
 
     imageObj.onload = function() {
         context.drawImage(imageObj, 100, 150);
+        context.drawImage(imageObj, 200, 300);
+
     };
     imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
+
+
 };
